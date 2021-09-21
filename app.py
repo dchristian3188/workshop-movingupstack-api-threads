6083b1e49c7bc79ba4f053c12313bed8d7c07389
David Christian <chradavi@amazon.com>
2021-09-20 22:26:56 -0700

    updated provisioning

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import json
+from logging import currentframe
 import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 import redis
 from db import db
 import mysql.connector
+from mysql.connector import errorcode
 
 app = Flask(__name__)
 cors = CORS(app, resources={f"*": {"origins": "*"}})
@@ -19,54 +21,69 @@ db_user = os.environ.get('DB_USER', 'root')
 db_passwd = os.environ.get('DB_PASSWORD', 'myAwesomePassword')
 db_name = os.environ.get('DATABASE', 'mydb')
 
-
-def create_db():
-    mydb = mysql.connector.connect(
+mydb = mysql.connector.connect(
         host=db_host,
         user=db_user,
         password=db_passwd
     )
+cursor = mydb.cursor()
 
-    mycursor = mydb.cursor()
-    mycursor.execute(f"CREATE DATABASE IF NOT EXISTS `mydb`;")
-    mycursor.execute("""
-        USE `mydb`;
 
-        CREATE TABLE IF NOT EXISTS `threads` (
+def create_db(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(db_name))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+try:
+    cursor.execute("USE {}".format(db_name))
+except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(db_name))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_db(cursor)
+        print("Database {} created successfully.".format(db_name))
+        mydb.database = db_name
+    else:
+        print(err)
+        exit(1)
+
+def create_table():
+    try:
+        print("Creating table threads")
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS `threads`(
             `id` int NOT NULL,
             `title` varchar(50) NOT NULL,
             `createdBy` int NOT NULL,
             PRIMARY KEY (`id`)
             );
+        """)
+        
+        cursor.execute("""
+            insert into `threads`(`id`,`title`,`createdBy`) 
+                values (5,'Threads up and running!',2);
+        """)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
 
-        """,multi=True)
-            
-
-    mycursor.execute("""    
-        USE `mydb`;
-
-        insert  into `threads`(`id`,`title`,`createdBy`) values
-            (1,'What''s up with the Lich?',1);
-
-        """,multi=True)
-
-create_db()
-conn = None
-if not local_db:
-    try:
-        conn = mysql.connector.connect(host=db_host, user=db_user, passwd=db_passwd, database=db_name)
-    except:
-        print("unable to connect to MySQL")
-
-
+create_table()
+    
 def get_from_db(table):
     if local_db:
         return db.get(table)
-
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM {table}")
-    return cursor.fetchall()
-
+    try:
+        cursor.execute(f"SELECT * FROM {table}")
+        return cursor.fetchall()
+    except Exception as error:
+        return "SQL error running: " + str(error)
+ 
 
 @app.route('/api/threads', methods=['GET'], strict_slashes=False)
 def threads():
@@ -83,23 +100,20 @@ def threads():
         else:
             body['source'] = 'redis'
             body['data'] = json.loads(value.decode('ascii'))
-            print("Body:")
+
+        print("Body:")
         print(body)
         return jsonify(body), 200
-
     except Exception as error: 
         print(error)
-        return error,200
-    
-
+        body['data'] = error
+        return str(error), 200
 
 @app.route('/api/threads/clear-cache', methods=['GET'], strict_slashes=False)
 def clear_cache():
-    red.delete("users")
-    red.delete("posts")
     red.delete("threads")
 
-    return "", 200
+    return "cleared threads", 200
 
 
 if __name__ == '__main__':
